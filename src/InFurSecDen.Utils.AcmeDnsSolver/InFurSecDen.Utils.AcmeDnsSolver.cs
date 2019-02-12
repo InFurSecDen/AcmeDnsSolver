4d1fa130dd7cb24bd2f2a707950dbcfee991cc19
TC Foxtaur <tcfox@tc.nz>
2019-02-12 22:32:29 +1300

    Init repo

---
+++ src/InFurSecDen.Utils.AcmeDnsSolver/InFurSecDen.Utils.AcmeDnsSolver.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace InFurSecDen.Utils.AcmeDnsSolver
+{
+    public static class InFurSecDen.Utils
+    {
+        [FunctionName("InFurSecDen.Utils")]
+    public static void Run([TimerTrigger("29 4 1 */2 *")]TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+    }
+}
+}
